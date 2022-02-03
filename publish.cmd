d1f627c08dcd06ac6138fb25411df9de3d852d16
Mahendra Lohar <91696273+Mahendra-Lohar@users.noreply.github.com>
2022-02-03 16:54:12 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\health-and-lifestyle-chatbot-bot-be2a-Web-Deploy.pubxml -p:Password=hMl0pr99zmAtbBnNf4FrxRree1xus2fLQmxBDqgPiCeCj735l3v6odK2ScsN
+
